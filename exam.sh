49fd0e07b74caf5c1397717af1b96be5f2b6a29c
zoeskan <zoeskandalakis@ucdavis.edu>
2025-01-23 11:12:35 -0800

    Update exam.sh

--- exam.sh
+++ exam.sh
@@ -14,5 +14,5 @@ live Assessment
 
 History:
 937  cd Code/MCB185/data
-938  zless jaspar2024_core.transfac.gz| grep "tax_group" | sort | uniq -c > exam.sh  940  nano exam.sh
+938  zless jaspar2024_core.transfac.gz| grep "tax_group" | sort | uniq -c > exam.sh nano exam.sh
 939  zless A.thaliana.gff.gz | cut -f7 | sort | uniq -c >> exam.sh
