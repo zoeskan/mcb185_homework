be9e4a9ca725a411a452028796f9cf834bfdda6e
zoeskan <zoeskandalakis@ucdavis.edu>
2025-02-15 13:39:55 -0800

    update

---
+++ 12phred.py
@@ -0,0 +1,20 @@
+# chars are fish not code <3
+
+def char_to_prob(x): return ord(x)
+
+print(char_to_prob('A'))
+print(char_to_prob('B'))
+print(char_to_prob('C'))
+print(char_to_prob('a'))
+print(char_to_prob('b'))
+print(char_to_prob('c'))
+
+def prob_to_char(x):
+	if 65 < x < 122:	
+		 return chr(int(x))
+	else:
+		return None
+	
+print(prob_to_char(0.001))
+print(prob_to_char(69))
+print(prob_to_char(101))
